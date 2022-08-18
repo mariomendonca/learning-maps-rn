f93ab87aad09b6b76c4fa494911ebada4c17e995
mario mendonca <mariomeendonca00@gmail.com>
2022-08-18 14:16:40 -0300

    remove file

--- src/routes/Drawer.tsx
+++ src/routes/Drawer.tsx
@@ -2,7 +2,6 @@ import { createDrawerNavigator } from "@react-navigation/drawer"
 import { Home } from "../screens/Home"
 import { NewMarkers } from "../screens/NewMarkers"
 import { PolygonCreate } from "../screens/PolygonCreate"
-import Testing from "../screens/Testing"
 
 export function Drawer() {
   const { Navigator, Screen } = createDrawerNavigator()
@@ -12,7 +11,6 @@ export function Drawer() {
       <Screen name="Home" component={Home} />
       <Screen name="NewMarkers" component={NewMarkers} />
       <Screen name="PolygonCreate" component={PolygonCreate} />
-      <Screen name="Testing" component={Testing} />
     </Navigator>
   )
 }
